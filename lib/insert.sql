4ebc78104f1e9c0b353b21f62259c9725796b329
Duke <dukemorise33@gmail.com>
2023-12-11 20:37:31 -0800

    completed lesson

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES 
+(1, "Mr. Chocolate", 34, "M", "Brown", "Angery", 1),
+(2, "Rowdy", 4, "M", "Black", "Happy", 1),
+(3, "Tabitha", 12, "F", "Brown", "Patient", 1),
+(4, "Sergeant Brown", 100, "M", "Brown", "Angery", 0),
+(5, "Melissa", 902, "F", "Black", "Reserved", 0),
+(6, "Grinch", 1009, "M", "Brown", "Angery", 0),
+(7, "Wendy", 23, "F", "Black", "Mature", 1),
+(8, NULL, 10000, "M", "Brown", "Berserker", 1);
